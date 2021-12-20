f0649b6a324f36942f634d42eb5817ad18d4942b
Mischa Reitsma <reitsma.mischa@gmail.com>
2021-12-20 20:32:06 +0100

    Super slow but working solution day19 part 2 2021

--- 2021/day19/solution.py
+++ 2021/day19/solution.py
@@ -8,7 +8,7 @@ from dataclasses import dataclass
 import collections
 import sys
 
-TEST: bool = True
+TEST: bool = False
 VERBOSE: bool = True
 DEBUG: bool = False
 
@@ -27,145 +27,81 @@ class Scanner:
         self.beacons: list[Vector] = beacons
         self.scanners: list[Vector] = []
 
-        self.distance_to_beacons: dict[int, dict[Vector, tuple[int]]] = {}
-        self.beacons_to_distance: dict[int, dict[tuple[int], Vector]] = {}
-        self.generate_distance_mapping()
-
         self.scanner_locations: dict[int, Vector] = {}
 
-        # Some convenience lists
-        self.overlap: dict[int, int] = {}
+        self.distances: dict[Vector, tuple[int]] = {}
 
-    def generate_distance_mapping(self):
-        self.distance_to_beacons = {}
-        self.beacons_to_distance = {}
-        _print(f'Generating a distance map for Scanner {self.id}')
-
-        for tmi , trans_matrix in enumerate(trans_3d_matrices):
-            self.distance_to_beacons[tmi] = {}
-            self.beacons_to_distance[tmi] = {}
-            if DEBUG:
-                _print(f'\tProcessing matrix {tmi}: {trans_matrix}')
-            for i, bi in enumerate(self.beacons):
-                if DEBUG:
-                    _print(f'\t\tProcessing vector {i}: {bi}')
-                for j, bj in enumerate(self.beacons[i+1:]):
-                    tbi = trans_matrix.transform_vector(bi)
-                    tbj = trans_matrix.transform_vector(bj)
-                    d = tbi - tbj
-                    s = (i, j)
-                    self.distance_to_beacons[tmi][d] = s
-                    self.beacons_to_distance[tmi][s] = d
-                    if DEBUG:
-                        _print(f'Distance between transformed vectors {i} ({bi}) and {j} ({bj}): {d}')
-
-    def add_beacons(self, beacons: list[Vector]):
-        for beacon in beacons:
-            for i, bi in enumerate(self.beacons):
-                # We only add for transformation 0
-                d = bi - beacon
-                s = (i, len(self.beacons))
-                self.distance_to_beacons[0][d] = s
-                self.beacons_to_distance[0][s] = d
-            self.beacons.append(beacon)
-
-    def add_distance_mapping(self):
-        # TODO: When we found the beacons to add, we also have the trans matrix, so then just add
-        # those.
-        pass
-
-    def find_overlaps(self, self_d2b: dict[Vector, set[int]], other_d2b: dict[Vector, set[int]]):
-        self.overlap = {}
-        for dist, beacons_indices in self_d2b.items():
-            for other_dist, other_beacon_indices in other_d2b.items():
-                # _print(f'Comparing distance vectors {dist} and {other_dist}')
-                if dist == other_dist:
-                    _print(f'dist == other_dist: {dist} == {other_dist}')
-                    self.overlap[beacons_indices[0]] = other_beacon_indices[0]
-                    self.overlap[beacons_indices[1]] = other_beacon_indices[1]
-                elif dist == -other_dist:
-                    _print(f'dist == -other_dist: {dist} == {-other_dist}')
-                    self.overlap[beacons_indices[0]] = other_beacon_indices[1]
-                    self.overlap[beacons_indices[1]] = other_beacon_indices[0]
-
-    def find_overlap_and_transformation_matrix(self, scanner: 'Scanner') -> int:
-        trans_index = 0
-        for i, d2b in scanner.distance_to_beacons.items():
-            _print(f'Trying transformation matrix {i+1}')
-            trans_index = i
-            # 0 index is unit matrix
-
-            if DEBUG:
-                _print(f'self.distance_to_beacons[0]: {self.distance_to_beacons[0]}')
-                _print(f'scanner.distance_to_beacons[{i}]: {d2b}')
-
-            self.find_overlaps(self.distance_to_beacons[0], d2b)
-
-            if DEBUG:
-                _print(self.overlap)
-
-            if len(self.overlap) >= 12:
-                break
-
-        if len(self.overlap) < 12:
-            trans_index = -1
-
-        # TODO: remove:
-        tmp_list1 = []
-        tmp_list2 = []
-        def format_vec(v):
-            return f'{v.x},{v.y},{v.z}'
-        for k, v in self.overlap.items():
-            b1 = self.beacons[k]
-            b2 = scanner.beacons[v]
-            tmp_list1.append(format_vec(b1))
-            tmp_list2.append(format_vec(b2))
-            _print(f'{k} vs {v}: {self.beacons[k]} and {scanner.beacons[v]}')
-
-        print('\n'.join(tmp_list1))
-        print('\n')
-        print('\n'.join(tmp_list2))
-
-        sys.exit()
-
-        return trans_index
+    def populate_distances(self):
+        self.distances = {}
+        for i, bi in enumerate(self.beacons):
+            for j, bj in enumerate(self.beacons[i+1:]):
+                if i == j:
+                    continue
+                self.distances[bi - bj] = (i, j)
 
     def process_scanner(self, scanner: 'Scanner') -> bool:
 
-        trans_index = self.find_overlap_and_transformation_matrix(scanner)
+        new_beacons: list[Vector] = []
+        scanner_location: Vector = None
 
+        if DEBUG:
+            _print('Distances: ')
+            for i in self.distances:
+                _print(i)
 
-        if trans_index == -1:
-            _print('Could not find any transformation matrix that made sense while processing scanner, returning False')
-            return False
+        for tmi, matrix in enumerate(trans_3d_matrices):
+            if DEBUG:
+                _print(f'\tTrying matrix {tmi}')
+            transformed_vecs = [matrix.transform_vector(v) for v in scanner.beacons]
+            scanner_location = None
+            for bi in transformed_vecs:
+                if scanner_location:
+                    break
+                for bj in transformed_vecs:
+                    if scanner_location:
+                        break
+                    if bi == bj:
+                        continue
+                    distance = bi - bj
+                    if DEBUG:
+                        _print(f'\t\t{distance}')
+                    in_distances = False
+                    for d in self.distances:
+                        if d == distance:
+                            in_distances = True
+                            break
+                    if in_distances:
+                        scanner_location = self.beacons[self.distances[distance][0]] - bi
+            overlap: list[Vector] = []
+            new_beacons = []
 
-        _print(f'Tranformation matrix found with id {id}')
+            if not scanner_location:
+                continue
 
-        idx = list(self.overlap.keys())[0]
+            for vec in transformed_vecs:
+                beacon_location = scanner_location + vec
+                if beacon_location in self.beacons:
+                    overlap.append(beacon_location)
+                else:
+                    new_beacons.append(beacon_location)
+            
+            if len(overlap) >= 12:
+                break
+            else:
+                new_beacons = []
 
-        beacon: Vector = self.beacons[idx]
-        original_other_beacon: Vector = scanner.beacons[self.overlap[idx]]
-        other_beacon: Vector = trans_3d_matrices[trans_index].transform_vector(original_other_beacon)
-        scanner_vector: Vector = beacon - other_beacon
+        if new_beacons:
+            self.scanner_locations[scanner.id] = scanner_location
+            self.scanners.append(scanner_location)
 
-        # if DEBUG:
-        _print(f'Original beacon: {beacon}')
-        _print(f'Original other beacon: {original_other_beacon}')
-        _print(f'Transformation matrix {trans_3d_matrices[trans_index]}')
-        _print(f'Transformed beacon: {other_beacon}')
-        _print(f'Location of processed scanner: {scanner_vector}')
+            for b in new_beacons:
+                self.beacons.append(b)
 
-        self.scanner_locations[scanner.id] = scanner_vector
+            self.populate_distances()
+            return True
 
-        new_beacons = []
-        skip_beacons = list(self.overlap.values())
-        # Vector pointing to scanner + (transformed) vector point to beacon is vector that points for s0 to beacon.
-        for b in enumerate([b for i, b in enumerate(scanner.beacons) if i not in skip_beacons]):
-            new_beacons.append(scanner_vector + trans_3d_matrices[trans_index].transform_vector(b))
+        return False
 
-        self.add_beacons(new_beacons)
-
-        return True
 
 def get_scanners() -> list[Scanner]:
     scanners: list[Scanner] = []
@@ -194,7 +130,9 @@ def exercise1() -> Scanner:
     s0 = scanners[0]
     del scanners[0]
 
-    prev_total = 5
+    s0.populate_distances()
+
+    prev_total = len(scanners) + 1
 
     while scanners:
         if prev_total == len(scanners):
@@ -206,15 +144,18 @@ def exercise1() -> Scanner:
             if s0.process_scanner(s):
                 scanners.remove(s)
                 break
+    for i, s in s0.scanner_locations.items():
+        print(f'Scanner {i}: {s}')
     return s0
 
 def exercise2(scanner: Scanner) -> int:
-    distances = 0
-    vectors = list(scanner.scanner_locations.values())[::]
-    vectors.append(Vector(0, 0, 0))
-    for i in range(len(vectors)):
-        for j in range(i + 1, len(vectors)):
-            distances.append(sum(vectors[i].distance(vectors[j])))
+    distances = []
+    for i, vi in enumerate(scanner.scanners):
+        for vj in scanner.scanners[i+1:]:
+            if vi == vj:
+                continue
+            distances.append(sum(vi.distance(vj)))
+
     return max(distances)
 
 if __name__ == "__main__":
