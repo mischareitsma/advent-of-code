974376d66664f732ff8a8c38209c95afab1669d7
Mischa Reitsma <reitsma.mischa@gmail.com>
2022-12-22 00:20:42 +0100

    Day 16 part 1 2022

--- 2022/day16/solution.py
+++ 2022/day16/solution.py
@@ -1,9 +1,8 @@
 # https://adventofcode.com/2022/day/16
-import itertools
 import os
 file_path = os.path.abspath(os.path.dirname(__file__))
 
-from queue import Queue
+from collections import deque
 
 TEST: bool = False
 
@@ -15,108 +14,118 @@ else:
 with open(INPUT_FILE, 'r') as f:
     LINES = [l.strip() for l in f.readlines()]
 
-class Valve:
-
-    def __init__(self, _id: str, rate: int, adjacent: list[str]):
-        self.id: str = _id
-        self.adjacent: list[str] = adjacent
-        self.rate: int = rate
-
-
-VALVES: dict[str, tuple] = {}
-SHORTEST_ROUTE: dict[tuple,list[str]] = {}
+valve_rates: dict[str, int] = {}
+valve_adjacent: dict[str, list[str]] = {}
 
 for l in LINES:
     valve = l.split(' ')[1]
-    rate = int(l.split(';')[0].split('=')[-1])
-    adjacent = l.split('valves' if 'tunnels' in l else 'valve')[-1].strip().split(', ') 
-    VALVES[valve] = (rate, adjacent)
+    valve_rates[valve] = int(l.split(';')[0].split('=')[-1])
+    valve_adjacent[valve] = l.split('valves' if 'tunnels' in l else 'valve')[-1].strip().split(', ') 
 
-# TODO: Should generalize BFS
-def get_shortest_route(s, e):
+valves_with_nonzero_rate: set[str] = set(k for k, v in valve_rates.items() if v > 0)
 
-    class Node:
-        def __init__(self, valve):
-            self.valve: str = valve
-            self.parent: str = None
-            self.explored: bool = False
-    
-    n: dict[str, Node] = {}
+shortest_path: dict[tuple[str], int] = {}
 
-    for v in VALVES:
-        n[v] = Node(v)
+# BFS to get shortest path
+def get_shortest_path(start: str, end: str) -> int:
 
-    q: Queue = Queue()
-    q.put(s)
-    n[s].explored = True
+    if start == end:
+        return 0
 
-    while True:
-        v = q.get()
-        if v == e:
-            break
+    queue: deque[list[str]] = deque()
+    visited: set[str] = set()
+    queue.append([start])
+    visited.add(start)
 
-        for a in [x for x in VALVES[v][1] if not n[x].explored]:
-            n[a].explored = True
-            n[a].parent = v
-            q.put(a)
+    while len(queue) > 0:
+        path = queue.popleft()
+        last_valve: str = path[-1]
+        for adjacent in [valve for valve in valve_adjacent[last_valve] if valve not in visited]:
+            if adjacent == end:
+                return len(path)
+            
+            visited.add(adjacent)
+            next_path = path[::]
+            next_path.append(adjacent)
+            queue.append(next_path)
 
-    l = []
-    iter = e
-    while n[iter].parent:
-        l.append(iter)
-        iter = n[iter].parent
-    l.append(iter)
-    l.reverse()
-    return l
+for start in valves_with_nonzero_rate:
+    for end in valves_with_nonzero_rate:
+        shortest_path[(start, end)] = get_shortest_path(start, end)
 
-VALVE_NAMES = list(VALVES.keys())
-RATE_VALVES = [k for k in VALVES.keys() if VALVES[k][0] > 0]
+for valve in valves_with_nonzero_rate:
+    shortest_path[('AA', valve)] = shortest_path[(valve, 'AA')] = get_shortest_path('AA', valve)
 
-for i in range(len(VALVES) - 1):
-    for j in range(i + 1, len(VALVES)):
-        vi = VALVE_NAMES[i]
-        vj = VALVE_NAMES[j]
-        SHORTEST_ROUTE[(vi, vj)] =  get_shortest_route(vi, vj)
-        # SHORTEST_ROUTE[(vj, vi)] = list(reversed(SHORTEST_ROUTE[(vi, vj)]))
+class Path:
 
-def get_route_length(start, finish):
-    return len(
-        SHORTEST_ROUTE[(start, finish)]
-        if (start, finish) in SHORTEST_ROUTE
-        else SHORTEST_ROUTE[(finish, start)]
-    )
+    def __init__(self):
+        self.valves: list[str] = []
+        self.opened_at_minute: dict[str, int] = {}
+        self.time_limit: int = 0
 
-def get_max_pressure():
-    max_pressure: int = 0
+    def next_path(self, valve: str) -> 'Path':
+        p: 'Path' = self.copy()
+        p.valves.append(valve)
+        previous_valve = self.valves[-1]
+        p.opened_at_minute[valve] = self.get_time_passed() + shortest_path[(previous_valve, valve)] + 1
+        return p
 
-    for path in itertools.permutations(RATE_VALVES):
+    def copy(self) -> 'Path':
+        p = Path()
+        p.valves = self.valves[::]
+        p.opened_at_minute = self.opened_at_minute.copy()
+        p.time_limit = self.time_limit
+        return p
+
+    def get_pressure(self) -> int:
+
         pressure = 0
-        activate = {}
-        pos = 'AA'
-        minutes = 0 # TODO or 1? or >= 30?
-        for step in path:
-            minutes+=get_route_length(pos, step)
-            if minutes > 30:
-                break
-            activate[step] = minutes
-            pos = step
-        for s, m in activate.items():
-            pressure += (30 - m) * VALVES[s][0]
-        
-        if pressure > max_pressure:
-            max_pressure = pressure
 
-    return max_pressure
+        for valve in [v for v in self.valves[1:] if self.opened_at_minute[v] < self.time_limit]:
+            pressure += (valve_rates[valve] * (self.time_limit - self.opened_at_minute[valve]))
 
+        return pressure
+
+    def get_time_passed(self) -> int:
+        last_valve: str = self.valves[-1]
+        return 0 if last_valve == 'AA' else self.opened_at_minute[last_valve]
+
+    def time_limit_exceeded(self) -> bool:
+        return self.get_time_passed() >= self.time_limit
+
+    def all_valves_opened(self) -> bool:
+        # -1 to correct for the 'AA'
+        return (len(self.valves) - 1) == len(valves_with_nonzero_rate)
+
+    @classmethod
+    def get_start(cls, time_limit: int):
+        p = cls()
+        p.valves.append('AA')
+        p.time_limit = time_limit
+
+        return p
 
 def part1():
+    max_pressure: int = 0
+    # DFS to get max
+    stack: deque[Path] = deque()
+    stack.append(Path.get_start(30))
 
-    max_pressure: int = get_max_pressure()
+    while len(stack) > 0:
+        current_path: Path = stack.pop()
 
-    print(f'Pressure released: {max_pressure}, goal: 1651')
+        if current_path.time_limit_exceeded() or current_path.all_valves_opened():
+            max_pressure = max(max_pressure, current_path.get_pressure())
+        else:
+            for valve in [v for v in valves_with_nonzero_rate if v not in current_path.valves]:
+                stack.append(current_path.next_path(valve))
+
+    print(f'Max pressure: {max_pressure}')
 
 def part2():
+    max_pressure: int = 0
     pass
+    
 
 def main():
     part1()
