2ec9ccf4a7fb6303685fb007b737cb1c210e8667
Mischa Reitsma <reitsma.mischa@gmail.com>
2023-01-06 17:46:31 +0100

    Redo of day 16 2022, now with part2

--- 2022/day16/solution.py
+++ 2022/day16/solution.py
@@ -1,11 +1,11 @@
 # https://adventofcode.com/2022/day/16
+from functools import cache
 import os
 file_path = os.path.abspath(os.path.dirname(__file__))
 
 from collections import deque
-from itertools import permutations
 
-TEST: bool =False
+TEST: bool = False
 
 if TEST:
     INPUT_FILE: str = f'{file_path}/test_input.dat'
@@ -15,202 +15,87 @@ else:
 with open(INPUT_FILE, 'r') as f:
     LINES = [l.strip() for l in f.readlines()]
 
-valve_rates: dict[str, int] = {}
-valve_adjacent: dict[str, list[str]] = {}
+def _get_rates_and_adjacent_valves() -> tuple[dict[str, int], dict[str, tuple[str,...]]]:
+    valve_rates: dict[str, int] = {}
+    valve_adjacent: dict[str, tuple[str,...]] = {}
 
-for l in LINES:
-    valve = l.split(' ')[1]
-    valve_rates[valve] = int(l.split(';')[0].split('=')[-1])
-    valve_adjacent[valve] = l.split('valves' if 'tunnels' in l else 'valve')[-1].strip().split(', ') 
+    for l in LINES:
+        valve = l.split(' ')[1]
+        valve_rates[valve] = int(l.split(';')[0].split('=')[-1])
+        valve_adjacent[valve] = tuple(l.split('valves' if 'tunnels' in l else 'valve')[-1].strip().split(', '))
 
-valves_with_nonzero_rate: set[str] = set(k for k, v in valve_rates.items() if v > 0)
+    return valve_rates, valve_adjacent
 
-shortest_path: dict[tuple[str], int] = {}
+VALVE_RATES, VALVE_ADJACENT = _get_rates_and_adjacent_valves()
 
-# BFS to get shortest path
-def get_shortest_path(start: str, end: str) -> int:
+# All valves with a non-zero rate
+VALVES: set[str] = set([k for k,v in VALVE_RATES.items() if v > 0])
 
+START_VALVE = 'AA'
+
+@cache
+def valve_shortest_route(start: str, end: str) -> int:
     if start == end:
         return 0
 
-    queue: deque[list[str]] = deque()
     visited: set[str] = set()
-    queue.append([start])
+
+    q: deque[list[str]] = deque()
+    q.append([start])
     visited.add(start)
 
-    while len(queue) > 0:
-        path = queue.popleft()
-        last_valve: str = path[-1]
-        for adjacent in [valve for valve in valve_adjacent[last_valve] if valve not in visited]:
-            if adjacent == end:
+    while len(q) > 0:
+        path = q.popleft()
+
+        for v in VALVE_ADJACENT[path[-1]]:
+            if v == end:
                 return len(path)
-            
-            visited.add(adjacent)
-            next_path = path[::]
-            next_path.append(adjacent)
-            queue.append(next_path)
+            visited.add(v)
+            new_path = path[::]
+            new_path.append(v)
+            q.append(new_path)
 
-for start in valves_with_nonzero_rate:
-    for end in valves_with_nonzero_rate:
-        shortest_path[(start, end)] = get_shortest_path(start, end)
+    raise ValueError(f'Could not find shortest path between {start} and {end}')
 
-for valve in valves_with_nonzero_rate:
-    shortest_path[('AA', valve)] = shortest_path[(valve, 'AA')] = get_shortest_path('AA', valve)
+def get_key(path: list[str]) -> str:
+    return ','.join(sorted(path))
 
-class Path:
+# TODO: Try bitmaps instead of lists, better data structure for this, also
+# easier for part 2, where we can just say `not (bitmap1 & bitmap2)`
+def explore(curr_valve: str, minutes: int, pressure: int, visited: list[str], paths: dict[str, int]):
+    key = get_key(visited)
+    paths[key] = max(paths.get(key, 0), pressure)
+    for valve in VALVES:
+        remaining_minutes = minutes - valve_shortest_route(curr_valve, valve) - 1
+        if (remaining_minutes <= 0) or (valve in visited):
+            continue
+        new_visited = visited[::]
+        new_visited.append(valve)
+        explore(valve, remaining_minutes, pressure + (VALVE_RATES[valve] * remaining_minutes), new_visited, paths)
 
-    def __init__(self):
-        self.valves: list[str] = []
-        self.opened_at_minute: dict[str, int] = {}
-        self.time_limit: int = 0
+    return paths
 
-    def next_path(self, valve: str) -> 'Path':
-        p: 'Path' = self.copy()
-        p.valves.append(valve)
-        previous_valve = self.valves[-1]
-        p.opened_at_minute[valve] = self.get_time_passed() + shortest_path[(previous_valve, valve)] + 1
-        return p
-
-    def copy(self) -> 'Path':
-        p = Path()
-        p.valves = self.valves[::]
-        p.opened_at_minute = self.opened_at_minute.copy()
-        p.time_limit = self.time_limit
-        return p
-
-    def get_pressure(self, time_limit: int = 0) -> int:
-
-        pressure = 0
-
-        if time_limit == 0:
-            time_limit = self.time_limit
-
-        for valve in [v for v in self.valves[1:] if self.opened_at_minute[v] < time_limit]:
-            pressure += (valve_rates[valve] * (time_limit - self.opened_at_minute[valve]))
-
-        return pressure
-
-    def get_time_passed(self) -> int:
-        last_valve: str = self.valves[-1]
-        return 0 if last_valve == 'AA' else self.opened_at_minute[last_valve]
-
-    def time_limit_exceeded(self) -> bool:
-        return self.get_time_passed() >= self.time_limit
-
-    def all_valves_opened(self) -> bool:
-        # -1 to correct for the 'AA'
-        return (len(self.valves) - 1) == len(valves_with_nonzero_rate)
-
-    @classmethod
-    def get_start(cls, time_limit: int):
-        p = cls()
-        p.valves.append('AA')
-        p.time_limit = time_limit
-
-        return p
-
-# Cache tuple, build during the first DFS run. Will store all combos
-# of paths that we find. Key is the ordered list of valves, value is
-# the pressure that is released. Replace pressure if pressure is larger
-# than other permutation of that path. Only paths that take less then 26
-# minutes are added.
-cache: dict[tuple[str,...],tuple[int, int]] = {}
-
-def add_to_cache(path: Path):
-    if path.get_time_passed() > 26:
-        return
-
-    # Tuple is unsorted, list is not hashable, so just use a string
-    key = ','.join(sorted(path.valves[1:]))
-    if len(key) == 0:
-        return
-
-    cache[key] = max(cache.get(key, 0), path.get_pressure(26))
-    
+def no_shared_valves(path1: str, path2: str):
+    for i in path1.split(','):
+        if i in path2:
+            return False
+    return True
 
 def part1():
-    max_pressure: int = 0
-    # DFS to get max
-    stack: deque[Path] = deque()
-    stack.append(Path.get_start(30))
-
-    while len(stack) > 0:
-        current_path: Path = stack.pop()
-
-        # Build up cache for part two:
-        add_to_cache(current_path)
-
-        if current_path.time_limit_exceeded() or current_path.all_valves_opened():
-            max_pressure = max(max_pressure, current_path.get_pressure())
-        else:
-            for valve in [v for v in valves_with_nonzero_rate if v not in current_path.valves]:
-                stack.append(current_path.next_path(valve))
-
-    print(f'Max pressure part 1: {max_pressure}')
-
-def get_available_valves(human_path: Path, elephant_path: Path) -> list[str]:
-    return [
-        v for v in valves_with_nonzero_rate
-        if v not in human_path.valves and v not in elephant_path.valves
-    ]
-
-def no_available_moves(human_path: Path, elephant_path: Path) -> bool:
-    return (
-        (human_path.time_limit_exceeded() and elephant_path.time_limit_exceeded())
-        or (len(human_path.valves) + len(elephant_path.valves) - 2 == len(valves_with_nonzero_rate))
-    )
-
-def part2_slow():
-    max_pressure: int = 0
-    
-    stack: deque[tuple[Path, Path]] = deque()
-    stack.append((Path.get_start(26), Path.get_start(26)))
-
-    while len(stack) > 0:
-        human_path, elephant_path = stack.pop()
-
-        if no_available_moves(human_path, elephant_path):
-            max_pressure = max(max_pressure, human_path.get_pressure() + elephant_path.get_pressure())
-        else:
-            for valve in get_available_valves(human_path, elephant_path):
-                if not human_path.time_limit_exceeded():
-                    stack.append((human_path.next_path(valve), elephant_path.copy()))
-                if not elephant_path.time_limit_exceeded():
-                    stack.append((human_path.copy(), elephant_path.next_path(valve)))
-
-    print(f'Max pressure part 2 slow: {max_pressure}')
+    paths = {}
+    explore(START_VALVE, 30, 0, [], paths)
+    pressure: int = max(paths.values())
+    print(f'Part 1: Pressure releases: {pressure} (should be {1651 if TEST else 1595})')
 
 def part2():
-    max_pressure: int = 0
-    for valves, pressure_route1 in cache.items():
-        # For sake of speed, ignore all routes where route 1 has less then
-        # a third of all valves. Bit hacky, but think this is a pretty
-        # educated guess that we need at least 1/3 of the valves per route.
-        valves_list = valves.split(',')
-        remaining_valves = ','.join(sorted([valve for valve in valves_with_nonzero_rate if valve not in valves_list]))
-
-        while len(remaining_valves) >= 2:
-            max_pressure = max(max_pressure, pressure_route1 + cache.get(remaining_valves, 0))
-            if max_pressure == 2042:
-                print('debug')
-            remaining_valves = remaining_valves[:remaining_valves.rfind(',')]
-
-
-        
-        # Could also be that not all valves can be reached, so try all combos:
-        # for i in range(len(remaining_valves) - 1):
-        #     max_pressure = max(max_pressure, cache.get(remaining_valves[:-i-1], 0))
-            # if max_pressure == 1237:
-            #     print('debug')
-
-    print(f'Max pressure part 2: {max_pressure}')
+    paths = {}
+    explore(START_VALVE, 26, 0, [], paths)
+    pressure: int = max(v1 + v2 for k1, v1 in paths.items() for k2, v2 in paths.items() if no_shared_valves(k1, k2))
+    print(f'Part 2: Pressure releases: {pressure} (should be {1707 if TEST else 2189})')
 
 def main():
     part1()
-    # Attempt too low: 2042
     part2()
-    if TEST:
-        part2_slow()
 
 if __name__ == "__main__":
     main()
