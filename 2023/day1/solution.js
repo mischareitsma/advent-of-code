494fb35ccee464a6f599f808f98ec6dbba79294c
Mischa Reitsma <reitsma.mischa@gmail.com>
2023-12-01 08:25:03 +0100

    2023 day 1

---
+++ 2023/day1/solution.js
@@ -0,0 +1,76 @@
+import * as fs from "node:fs";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const isTest = false;
+const testNumber = 2;
+
+const fname = (isTest ? `test${testNumber}_` : '') + 'input.dat'
+
+const lines = fs.readFileSync(__dirname + "/" + fname).toString().split('\n');
+lines.pop()
+
+async function part1() {
+	let sum = 0;
+
+	lines.forEach(line => {
+		let firstDigit = -1, lastDigit = -1;
+		for (let i = 0; firstDigit === -1; i++) {
+			const char = line.charAt(i);
+			const n = Number.parseInt(char);
+			if (n || n === 0) firstDigit = n;
+		}
+		for (let i = line.length - 1; lastDigit === -1; i--) {
+			const char = line.charAt(i);
+			const n = Number.parseInt(char);
+			if (n || n === 0) lastDigit = n;
+		}
+		sum += firstDigit * 10 + lastDigit;
+	});
+	return sum;
+}
+
+async function part2() {
+	const digits = [
+		'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+		'0', '1' ,'2', '3', '4', '5', '6', '7', '8', '9'
+	];
+
+	let sum = 0;
+
+	lines.forEach((line, ln) => {
+		let firstIdx = line.length
+		let firstDigit = 0
+		let lastIdx = -1
+		let lastDigit = 0
+		digits.forEach((v, i) => {
+			let firstIdxForDigit = line.indexOf(v)
+			let lastIdxForDigit = line.lastIndexOf(v);
+			if (firstIdxForDigit === -1 && lastIdxForDigit === -1 ) return;
+			if (firstIdxForDigit < firstIdx) {
+				firstIdx = firstIdxForDigit
+				firstDigit = i % 10;
+			}
+			if (lastIdxForDigit > lastIdx) {
+				lastIdx = lastIdxForDigit;
+				lastDigit = i % 10;
+			}
+		});
+
+		sum += firstDigit * 10 + lastDigit
+	});
+
+	return sum;
+}
+
+
+async function main() {
+	console.log(`Part 1: ${await part1()}`);
+	console.log(`Part 2: ${await part2()}`);
+}
+
+main().then().catch(
+	err => console.error(err)
+);
