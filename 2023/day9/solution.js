19af5d96e0a29fc2c7b3a16afbb430f2f7c02cb6
Mischa Reitsma <reitsma.mischa@gmail.com>
2023-12-09 14:29:57 +0100

    2023 day 9

---
+++ 2023/day9/solution.js
@@ -0,0 +1,48 @@
+import * as fs from "node:fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const isTest = false;
+
+const fname = (isTest ? `test_` : "") + "input.dat";
+
+const lines = fs.readFileSync(__dirname + "/" + fname).toString().split('\n');
+lines.pop();
+
+const digits = lines.map(line => line.split(" ").map(d => Number.parseInt(d)));
+
+function getNextInSequence(digits, getPrevious) {
+	const deltas = [];
+	for (let i = 0; i < digits.length - 1; i++) {
+		deltas.push(digits[i+1] - digits[i]);
+	}
+
+	if (deltas.filter(d => d !== 0).length === 0) return digits[digits.length - 1];
+
+	return getPrevious
+		? (digits[0] - getNextInSequence(deltas, getPrevious))
+		: (digits[digits.length - 1] + getNextInSequence(deltas, getPrevious));
+}
+
+function part1() {
+	return sumNextDigitsInSequence(false);
+}
+
+function part2() {
+	return sumNextDigitsInSequence(true)
+}
+
+function sumNextDigitsInSequence(getPrevious) {
+	return digits.map(d => getNextInSequence(d, getPrevious)).reduce((a, b) => a+b);
+}
+
+async function main() {
+	console.log(`Part 1: ${part1()}`);
+	console.log(`Part 2: ${part2()}`);
+}
+
+main().then().catch(
+	err => console.error(err)
+);
