6818164afeb1be6ccd5e746160403ec7b341cbf8
Mischa Reitsma <reitsma.mischa@gmail.com>
2024-12-25 00:32:51 +0100

    2024 day 24 part 1, part 2 to slow

--- 2024/day24.py
+++ 2024/day24.py
@@ -1,51 +1,96 @@
 import os
-TEST: bool = True
+TEST: bool = False
 TEST_PART: int = 2
 
 FILE_NAME = f"day24_test_input{TEST_PART}.dat" if TEST else "day24_input.dat"
 FILE_PATH = f'{os.path.dirname(os.path.realpath(__file__))}/{FILE_NAME}'
 
-wires = {}
-gates = {}
-
-gates_processed = set()
-all_wires = set()
+WIRES = {}
+GATES = {}
 
 for w_input, g_input in (open(FILE_PATH).read().split("\n\n"),):
     for w in w_input.strip().split("\n"):
         wire = w.split(": ")[0]
-        wires[wire] = bool(int(w.split(": ")[-1]))
-        all_wires.add(wire)
+        WIRES[wire] = bool(int(w.split(": ")[-1]))
     
     for gate in g_input.strip().split("\n"):
-        # x00 AND y00 -> z00
         w1, g, w2, _, w3 = gate.split()
-        gates[(w1, w2)] = (g, w3)
-        all_wires.add(w1)
-        all_wires.add(w2)
-        all_wires.add(w3)
+        GATES[w3] = (w1, g, w2)
 
+def part1():
+    print(get_number(GATES.copy(), WIRES.copy()))
 
-while len(all_wires) != len(wires):
-    print(wires)
-    for g in [_ for _ in gates.keys() if g not in gates_processed]:
-        w1, w2 = g
-        if not (w1 in wires and w2 in wires):
-            continue
-        v, w3 = gates[g]
-        wv1 = wires[w1]
-        wv2 = wires[w2]
-        wv3 = None
-        if v == "AND":
-            wv3 = wv1 and wv2
-        if v == "OR":
-            wv3 = wv1 or wv2
-        if v == "XOR":
-            wv3 = wv1 != wv2
+def get_number_for_swapped_pairs(pairs):
+    gates = dict(GATES)
+    for w1, w2 in pairs:
+        gates[w1], gates[w2] = gates[w2], gates[w1]
+    return get_number(gates, dict(WIRES))
+
+def get_number(gates, wires):
+    while gates:
+        kill_gates = []
+        for w3, (w1, g, w2) in gates.items():
         
-        wires[w3] = wv3
-        gates_processed.add(g)
+            if w3 in wires:
+                continue
+            if w1 not in wires and w2 not in wires:
+                continue
 
-p1 = ''.join(str(int(wires[_])) for _ in sorted(_ for _ in all_wires if _.startswith("z")))[::-1]
+            if w1 in wires and w2 in wires:
+                w1v = wires[w1]
+                w2v = wires[w2]
 
-print(int(p1, 2))
+                if g == "XOR":
+                    wires[w3] = (w1v != w2v)
+                    kill_gates.append(w3)
+                    continue
+                if g == "OR":
+                    wires[w3] = w1v or w2v
+                    kill_gates.append(w3)
+                    continue
+                if g == "AND":
+                    wires[w3] = w1v and w2v
+                    kill_gates.append(w3)
+                    continue
+            if( w1 in wires and w2 not in wires) or (w1 not in wires and w2 in wires):
+                wv = wires[w1] if w1 in wires else wires[w2]
+                if g == "AND" and wv == False:
+                    wires[w3] = False
+                    kill_gates.append(w3)
+                    continue
+                if g == "OR" and wv:
+                    wires[w3] = True
+                    kill_gates.append(w3)
+        for kg in kill_gates:
+            del gates[kg]
+
+    n = ''.join(str(int(wires[_])) for _ in sorted(_ for _ in wires.keys() if _.startswith("z")))[::-1]
+    return int(n, 2)
+
+def part2():
+    xbin = ''.join(str(int(WIRES[_])) for _ in sorted(_ for _ in WIRES.keys() if _.startswith("x")))[::-1]
+    ybin = ''.join(str(int(WIRES[_])) for _ in sorted(_ for _ in WIRES.keys() if _.startswith("x")))[::-1]
+    x = int(xbin, 2)
+    y = int(ybin, 2)
+
+    pairs = set()
+    print(GATES)
+    for x in GATES:
+        for y in GATES:
+            if x == y:
+                continue
+            pairs.add(tuple(sorted([x, y])))
+    pairs = list(pairs)
+    print(len(pairs))
+    for i, p1 in enumerate(pairs[:-3]):
+        for j, p2 in enumerate(pairs[i+1:-2]):
+            for k, p3 in enumerate(pairs[i+j+2:-1]):
+                for p4 in pairs[i+j+k+3:]:
+                    z = get_number_for_swapped_pairs([p1, p2, p3, p4])
+                    if x + y == z:
+                        print(','.join(list(sum([p1, p2, p3, p4], ()))))
+                        return
+
+if __name__ == "__main__":
+    part1()
+    part2()
