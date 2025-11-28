2be926fe5161496ef7d1e8edca8d4c21b93d25d5
Mischa Reitsma <reitsma.mischa@gmail.com>
2025-11-28 11:21:37 +0100

    Prep for 2025

---
+++ 2025/day00.py
@@ -0,0 +1,46 @@
+# day00.py template
+import os
+import sys
+
+TEST: bool = "-t" in sys.argv
+DAY: str = os.path.basename(__file__)[3:5]
+
+def info_from_args() -> tuple[str, int]:
+    n: str = ""
+    p: int = 0
+    for arg in sys.argv:
+        if arg.startswith("-n"):
+            n = arg[2:]
+        if arg.startswith("-p"):
+            p=int(arg[2])
+    if p not in (0, 1, 2):
+        p = 0
+    return n, p
+
+N, P = info_from_args()
+
+FILE_NAME = f"day{DAY}{"_test" if TEST else ""}_input{N}.dat"
+FILE_PATH = f'{os.path.dirname(os.path.realpath(__file__))}/{FILE_NAME}'
+
+def print_msg():
+    msg: str = f"Running day {int(DAY)}"
+    if P > 0:
+        msg += f" part {P}"
+    if TEST:
+        msg += " in test mode"
+
+    print(msg)
+    print(f"Input file used for solution: {FILE_NAME}")
+
+def part1():
+    pass
+
+def part2():
+    pass
+
+if __name__ == "__main__":
+    print_msg()
+    if P in (0, 1):
+        print(f"part 1: {part1()}")
+    if P in (0, 2):
+        print(f"part 2: {part2()}")
